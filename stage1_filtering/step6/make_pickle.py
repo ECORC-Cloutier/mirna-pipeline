4b469b80198e773ff7b407fa34b08bd708d07a11
AAFC <dhdouglas.huang@gmail.com>
2015-03-13 14:16:10 -0400

    change comment instruction

--- stage1_filtering/step6/make_pickle.py
+++ stage1_filtering/step6/make_pickle.py
@@ -1,9 +1,9 @@
 import cPickle
 
-days = [0,1,3,7,10] #change the days if necessary - only ADD days DO NOT remove days; extra pickle files can be deleted later
+days = [0,1,3,7,10] #change the days if necessary
 dump = dict()
 
-for i in range(0,5): #if days were changed, change the upper bound of the range to the number of days present
+for i in range(0,5): #if days were changed, change the upper bound of the range to the NUMBER of days present NOTE: day0 is a day!
     pick = open ('day' + str(days[i]) + '.pickle','w')
     cPickle.dump ( dump, pick)
     pick.close()
