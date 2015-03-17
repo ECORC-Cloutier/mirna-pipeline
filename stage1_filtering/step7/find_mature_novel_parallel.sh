050653c6165964a6563ab278ad43fa8a5a67dc68
AAFC <dhdouglas.huang@gmail.com>
2015-03-17 08:35:03 -0400

    change wildcard

--- stage1_filtering/step7/find_mature_novel_parallel.sh
+++ stage1_filtering/step7/find_mature_novel_parallel.sh
@@ -4,4 +4,4 @@ source main.sh
 
 export -f find_mature_novel
 
-parallel find_mature_novel {} ::: *.cons_master  #change wildcard if necessary
+parallel find_mature_novel {} ::: *cons_master  #change wildcard if necessary
